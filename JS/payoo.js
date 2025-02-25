4a3c0593d975b83291cbfb6f44cd054939d98f6c
Sabbir-75 <kazisabbir4361@gmail.com>
2025-02-26 00:55:27 +0600

    updated file name

--- JS/payoo.js
+++ JS/payoo.js
@@ -7,7 +7,7 @@ document.getElementById("btn_lgn").addEventListener("click", function(event){
 
     if(numberId.length === 11 && parseInt(numberId[0]) === 0 && parseInt(numberId[1]) === 1 && parseInt(numberId[2]) === 9 && parseInt(numberId[10]) === 2){
         if(pinId.length === 4 && pinId === "9200"){
-           window.location.href = "../html 2/index.html";
+           window.location.href = "main.html";
         }
         else{
             alert("Required Pin Number")
